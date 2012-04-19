0b930e4860eef45c49275aa9df791bc36f17e4c5
Philip Clarke <philip.clarke@cylon.com>
2012-04-19 23:55:53 +0100

    update supervisor to use simple_one_for_one

--- src/reverse_proxy_sup.erl
+++ src/reverse_proxy_sup.erl
@@ -11,12 +11,13 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_child/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
+-define(LISTEN_PORT, 4900).
 
 %%%===================================================================
 %%% API functions
@@ -50,7 +51,10 @@ start_link() ->
 %% @end
 %%--------------------------------------------------------------------
 init([]) ->
-        RestartStrategy = one_for_one,
+        %% this callback will start up a number of children as well as returning the spec
+        %% the number of children will be the number of entries in the config file
+        %% otherwise 5 children will be started.  The config file is read in by the application
+        RestartStrategy = simple_one_for_one,
         MaxRestarts = 1000,
         MaxSecondsBetweenRestarts = 3600,
 
@@ -60,14 +64,31 @@ init([]) ->
         Shutdown = 2000,
         Type = worker,
 
-        AChild = {reverse_proxy, {reverse_proxy, start_link, []},
-                          Restart, Shutdown, Type, [reverse_proxy]},
+        {ok, ListenSock} = gen_tcp:listen(?LISTEN_PORT, [binary, {packet, 0}, {active, true}, {reuseaddr, true}]),
+        AChild = {reverse_proxy, 
+                  {reverse_proxy, start_link, [ListenSock]},
+                  Restart, Shutdown, Type, [reverse_proxy]},
 
+        spawn_link(fun start_all_children/0),
         {ok, {SupFlags, [AChild]}}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
+start_child() ->
+    supervisor:start_child(reverse_proxy_sup, []).
+
+
+start_all_children() ->
+    NumConnections = 
+    case ets:info(reverse_proxy_config, size) of
+        undefined ->  5;
+        N -> N
+    end,
+        
+    io:format("~p~n", [NumConnections]),
+    [start_child() || _ <- lists:seq(1, NumConnections)],
+    ok.
 
 
